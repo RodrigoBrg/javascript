6725d8f526251a1b652a5d88dd35a44718b7e627
Rodrigo Braga <113397418+RodrigoBrg@users.noreply.github.com>
2023-01-12 17:41:27 -0300

    create ex 09 and 010

---
+++ aula011/ex08.js
@@ -0,0 +1,10 @@
+var vel = 8.6
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 10){
+console.log('Voçê ultrapassou a velocidade permitida. MULTADO!')
+
+}
+else{
+    console.log('Continue assim, você está dentro da velocidade permitida!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
