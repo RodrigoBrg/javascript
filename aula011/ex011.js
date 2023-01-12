3238a9fdbedea7b9febb922623e0a7b724149a59
Rodrigo Braga <113397418+RodrigoBrg@users.noreply.github.com>
2023-01-12 19:20:45 -0300

    Create ex011.js

---
+++ aula011/ex011.js
@@ -0,0 +1,9 @@
+var idade = 66
+console.log(`Sua idade é ${idade} anos`)
+if (idade < 16){
+    console.log('não vota!')
+} else if(idade < 18 || idade > 65 ){
+    console.log('Seu voto é opcional!')}
+    else{
+    console.log('Seu voto é obrigatório')  
+}
