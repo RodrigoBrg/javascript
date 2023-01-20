f134ab3961da4dca61d1920eda207f9b07fde7d3
Rodrigo Braga <113397418+RodrigoBrg@users.noreply.github.com>
2023-01-20 19:53:02 -0300

    Update script.js

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -6,16 +6,23 @@ function Contar(){
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     }else {
-
         res.innerHTML = 'Contando: '
-
         let i = Number(ini.value);
         let f = Number(fim.value);
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c+= p){
-            res.innerHTML += ` ${c} \u{1F449}`
+        if(i < f) {
+            //Contagem progressiva
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else{
+            //Contagem regressiva
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
         }
+        res.innerHTML += `\u{1F3C1}`
     }   
 }
